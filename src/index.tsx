f1ae2c8ca89e3ac55cf1e914481541d4e490eca2
Ricardo Saumeth <ricardosantana46@hotmail.com>
2025-12-04 19:45:25 +0000

    move agConfig

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
-import App from "./App"
-import "../config/agGridConfig"
 import "ag-grid-community/styles/ag-theme-quartz.css"
+import "./config/agGridConfig"
+import App from "./App"
 import "./theme/fonts.css"
 
 const container = document.getElementById("root")!
